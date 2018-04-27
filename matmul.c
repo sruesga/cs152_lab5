cb6d079cb61021e2eaf0926ae0094ac804ea21fe
Skyler Ruesga <sruesga7323@gmail.com>
2018-04-27 14:20:15 -0700

    changed vvadd

--- matmul.c
+++ matmul.c
@@ -20,44 +20,44 @@ void matmul(const size_t coreid, const size_t ncores, const size_t lda,  const d
 
 void matmul_opt(const size_t coreid, const size_t ncores, const size_t lda,  const data_t A[], const data_t B[], data_t C[])
 {
-  size_t i, j, k;
+  size_t i, k;
   size_t end = ((coreid + 1) * lda) / ncores;
 
   for (i = (coreid * lda) / ncores; i < end; i++) {
     for (k = 0; k < lda; k++) {
       data_t elem_a = A[i*lda + k];
-         C[i*lda + j] += elem_a * B[k*lda + j];
-         C[i*lda + j + 1] += elem_a * B[k*lda + j + 1];
-         C[i*lda + j + 2] += elem_a * B[k*lda + j + 2];
-         C[i*lda + j + 3] += elem_a * B[k*lda + j + 3];
-         C[i*lda + j + 4] += elem_a * B[k*lda + j + 4];
-         C[i*lda + j + 5] += elem_a * B[k*lda + j + 5];
-         C[i*lda + j + 6] += elem_a * B[k*lda + j + 6];
-         C[i*lda + j + 7] += elem_a * B[k*lda + j + 7];
-         C[i*lda + j + 8] += elem_a * B[k*lda + j + 8];
-         C[i*lda + j + 9] += elem_a * B[k*lda + j + 9];
-         C[i*lda + j + 10] += elem_a * B[k*lda + j + 10];
-         C[i*lda + j + 11] += elem_a * B[k*lda + j + 11];
-         C[i*lda + j + 12] += elem_a * B[k*lda + j + 12];
-         C[i*lda + j + 13] += elem_a * B[k*lda + j + 13];
-         C[i*lda + j + 14] += elem_a * B[k*lda + j + 14];
-         C[i*lda + j + 15] += elem_a * B[k*lda + j + 15];
-         C[i*lda + j + 16] += elem_a * B[k*lda + j + 16];
-         C[i*lda + j + 17] += elem_a * B[k*lda + j + 17];
-         C[i*lda + j + 18] += elem_a * B[k*lda + j + 18];
-         C[i*lda + j + 19] += elem_a * B[k*lda + j + 19];
-         C[i*lda + j + 20] += elem_a * B[k*lda + j + 20];
-         C[i*lda + j + 21] += elem_a * B[k*lda + j + 21];
-         C[i*lda + j + 22] += elem_a * B[k*lda + j + 22];
-         C[i*lda + j + 23] += elem_a * B[k*lda + j + 23];
-         C[i*lda + j + 24] += elem_a * B[k*lda + j + 24];
-         C[i*lda + j + 25] += elem_a * B[k*lda + j + 25];
-         C[i*lda + j + 26] += elem_a * B[k*lda + j + 26];
-         C[i*lda + j + 27] += elem_a * B[k*lda + j + 27];
-         C[i*lda + j + 28] += elem_a * B[k*lda + j + 28];
-         C[i*lda + j + 29] += elem_a * B[k*lda + j + 29];
-         C[i*lda + j + 30] += elem_a * B[k*lda + j + 30];
-         C[i*lda + j + 31] += elem_a * B[k*lda + j + 31];
+      C[i*lda] += elem_a * B[k*lda];
+      C[i*lda + 1] += elem_a * B[k*lda + 1];
+      C[i*lda + 2] += elem_a * B[k*lda + 2];
+      C[i*lda + 3] += elem_a * B[k*lda + 3];
+      C[i*lda + 4] += elem_a * B[k*lda + 4];
+      C[i*lda + 5] += elem_a * B[k*lda + 5];
+      C[i*lda + 6] += elem_a * B[k*lda + 6];
+      C[i*lda + 7] += elem_a * B[k*lda + 7];
+      C[i*lda + 8] += elem_a * B[k*lda + 8];
+      C[i*lda + 9] += elem_a * B[k*lda + 9];
+      C[i*lda + 10] += elem_a * B[k*lda + 10];
+      C[i*lda + 11] += elem_a * B[k*lda + 11];
+      C[i*lda + 12] += elem_a * B[k*lda + 12];
+      C[i*lda + 13] += elem_a * B[k*lda + 13];
+      C[i*lda + 14] += elem_a * B[k*lda + 14];
+      C[i*lda + 15] += elem_a * B[k*lda + 15];
+      C[i*lda + 16] += elem_a * B[k*lda + 16];
+      C[i*lda + 17] += elem_a * B[k*lda + 17];
+      C[i*lda + 18] += elem_a * B[k*lda + 18];
+      C[i*lda + 19] += elem_a * B[k*lda + 19];
+      C[i*lda + 20] += elem_a * B[k*lda + 20];
+      C[i*lda + 21] += elem_a * B[k*lda + 21];
+      C[i*lda + 22] += elem_a * B[k*lda + 22];
+      C[i*lda + 23] += elem_a * B[k*lda + 23];
+      C[i*lda + 24] += elem_a * B[k*lda + 24];
+      C[i*lda + 25] += elem_a * B[k*lda + 25];
+      C[i*lda + 26] += elem_a * B[k*lda + 26];
+      C[i*lda + 27] += elem_a * B[k*lda + 27];
+      C[i*lda + 28] += elem_a * B[k*lda + 28];
+      C[i*lda + 29] += elem_a * B[k*lda + 29];
+      C[i*lda + 30] += elem_a * B[k*lda + 30];
+      C[i*lda + 31] += elem_a * B[k*lda + 31];
     }
   }
 }
--- vvadd.c
+++ vvadd.c
@@ -22,9 +22,22 @@ void __attribute__((noinline)) vvadd_opt(int coreid, int ncores, size_t n, const
   // TODO: Your code here
   size_t i = (coreid * n) / ncores;
   size_t end = ((coreid + 1) * n) / ncores;
-
-  for (; i < end; i+=1) 
+  size_t unroll_end = end - end%10;
+  for (; i < unroll_end; i+=10)
   {
-	z[i] = x[i] + y[i];
+    z[i] = x[i] + y[i];
+    z[i + 1] = x[i + 1] + y[i + 1];
+    z[i + 2] = x[i + 2] + y[i + 2];
+    z[i + 3] = x[i + 3] + y[i + 3];
+    z[i + 4] = x[i + 4] + y[i + 4];
+    z[i + 5] = x[i + 5] + y[i + 5];
+    z[i + 6] = x[i + 6] + y[i + 6];
+    z[i + 7] = x[i + 7] + y[i + 7];
+    z[i + 8] = x[i + 8] + y[i + 8];
+    z[i + 9] = x[i + 9] + y[i + 9];
+  }
+  for(i = unroll_end; i < end; i++)
+  {
+    z[i] = x[i] + y[i];
   }
 }
